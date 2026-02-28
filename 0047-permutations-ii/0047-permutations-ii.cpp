f4d929e08f9bd8e9df1e7d701edbd5f77445aea9
thakurpuru <purushottamthakur78@gmail.com>
2026-02-28 16:07:47 +0530

    0047-PERMUTATIONS-II Stats: Time: 1 ms (74.73%), Space: 11.9 MB (61.75%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     vector<bool> vis;
-    set<vector<int>> ans;
+    vector<vector<int>> ans;
     void permutation(vector<int>& nums,vector<int>& path,int i){
         if(i==nums.size()){
-            ans.insert(path);
+            ans.push_back(path);
             return;
         }
         for(int j=0;j<nums.size();j++){
+            if(j>0 && nums[j]==nums[j-1] && !vis[j-1])continue;
             if(!vis[j]){
                 path.push_back(nums[j]);
                 vis[j]=true;
@@ -21,6 +22,7 @@ public:
         vector<int> path;
         int n=nums.size();
         vis.assign(n,false);
+        sort(nums.begin(),nums.end());
         permutation(nums,path,0);
         return vector<vector<int>>(ans.begin(),ans.end());
     }
