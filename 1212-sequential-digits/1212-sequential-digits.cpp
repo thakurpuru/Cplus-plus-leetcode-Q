76963dfa88e6015a35003861ad399f3b12e79f75
thakurpuru <purushottamthakur78@gmail.com>
2026-03-03 01:28:06 +0530

    1212-SEQUENTIAL-DIGITS Stats: Time: 0 ms (100%), Space: 8.2 MB (75.19%) - LeetHub

--- 1212-sequential-digits/1212-sequential-digits.cpp
+++ 1212-sequential-digits/1212-sequential-digits.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    // void solve(long long num,int low,int high,int i){
-    //     if(num>high){
-    //         return;
-    //     }
-    //     if(num>low && num<high){
-    //         ans.push_back(num);
-    //         return;
-    //     }
-    //     for(int j=i;j<=9;j++){
-    //         long long res=(num*10)+j;
-    //         solve(res,low,high,j+1);
-    //     }
-
-    // }
+    
+    void solve(long long num,int low,int high,int j,vector<int>& ans){
+        if(num>=low && num<=high){
+            ans.push_back(num);
+        }
+        if(num>high || j>9){
+            return;
+        }
+       
+            
+        solve(num*10+j,low,high,j+1,ans);
+        
+        return;
+    }
     vector<int> sequentialDigits(int low, int high) {
         vector<int> ans;
-
-        for(int i=1;i<=9;i++){
-            int num=i;
-            int dig=i+1;
-            while(num<=high && dig<=9){
-                num=(num*10)+dig;
-                if(num>=low && num<=high) ans.push_back(num);
-                dig++;
-            }
+        for(long long i=1;i<=9;i++){
+            solve(i,low,high,i+1,ans);
         }
+        
         sort(ans.begin(),ans.end());
         return ans;
     }
