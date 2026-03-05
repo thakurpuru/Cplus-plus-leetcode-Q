dae724a4585b6454536ff3fabba544b2e56b2caa
thakurpuru <purushottamthakur78@gmail.com>
2026-03-05 17:22:07 +0530

    1884-MINIMUM-CHANGES-TO-MAKE-ALTERNATING-BINARY-STRING Stats: Time: 0 ms (100%), Space: 9.4 MB (76.2%) - LeetHub

--- 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
     int minOperations(string s) {
-        int bit=0;
+        int bit=0,bit1=1;
         int count1=0;
-        for(int i=0;i<s.size();i++){
-            int ch=s[i]-'0';
-            if(ch^bit==1) count1++;
-            bit^=1;
-        }
-        bit=1;
         int count2=0;
         for(int i=0;i<s.size();i++){
             int ch=s[i]-'0';
-            if(ch^bit==1) count2++;
+            if(ch^bit==1) count1++;
+            if(ch^bit1==1) count2++;
+            bit1^=1;
             bit^=1;
         }
-        cout<<count1<<count2;
+       
         return min(count1,count2);
     }
 };
