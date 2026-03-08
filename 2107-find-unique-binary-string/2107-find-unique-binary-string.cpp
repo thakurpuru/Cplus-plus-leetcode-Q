42aabb46936ef00866c47a90ea74e79acced9a6b
thakurpuru <purushottamthakur78@gmail.com>
2026-03-08 16:38:22 +0530

    2107-FIND-UNIQUE-BINARY-STRING Stats: Time: 0 ms (100%), Space: 12.5 MB (86.66%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
-    string ans;
-    void solve(unordered_set<string> &st,string &s,int start,int n,string s1){
-        if(s1.size()==n){
-            if(!st.count(s1)){
-                ans=s1;
-            }
-            return ;
-        }
-        for(int i=start;i<s.size();i++){
-            solve(st,s,i,n,s1+s[i]);
-        }
-    }
+    // string ans;
+    // void solve(unordered_set<string> &st,string &s,int start,int n,string s1){
+    //     if(s1.size()==n){
+    //         if(!st.count(s1)){
+    //             ans=s1;
+    //         }
+    //         return ;
+    //     }
+    //     for(int i=start;i<s.size();i++){
+    //         solve(st,s,i,n,s1+s[i]);
+    //     }
+    // }
     string findDifferentBinaryString(vector<string>& nums) {
-        string s="01";
-        ans="";
-        int n=nums.size();
-        unordered_set<string> st(nums.begin(),nums.end());
-        solve(st,s,0,n,"");
+        // string s="01";
+        // ans="";
+        // int n=nums.size();
+        // unordered_set<string> st(nums.begin(),nums.end());
+        // solve(st,s,0,n,"");
+
+        string ans="";
+        for(int i=0;i<nums.size();i++){
+            if(nums[i][i]=='1')ans+='0';
+            else ans+='1';
+        }
         return ans;
     }
 };
