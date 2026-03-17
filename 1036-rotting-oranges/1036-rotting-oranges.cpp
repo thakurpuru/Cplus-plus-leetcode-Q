2c11a10b0ab7e902de44cdf6fd7584d2f0dc2168
thakurpuru <purushottamthakur78@gmail.com>
2026-03-17 14:04:49 +0530

    1036-ROTTING-ORANGES Stats: Time: 1 ms (37.31%), Space: 16.6 MB (86.74%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,50 +1,44 @@
 class Solution {
 public:
-    
-    int orangesRotting(vector<vector<int>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
+    int bfs(vector<vector<int>>& grid,queue<pair<int,int>>& q,int &fresh){
         int ans=0;
+        int m=grid.size(),n=grid[0].size();
+        int dx[4]={1,-1,0,0};
+        int dy[4]={0,0,1,-1};
+        while(!q.empty() && fresh>0){
+            int size=q.size();
+            ans++;
+            for(int i=0;i<size;i++){
+                auto [x,y]=q.front();
+                q.pop();
+                for(int k=0;k<4;k++){
+                    int i=x+dx[k],j=y+dy[k];
+                    if(i>=0 && j>=0 && i<m && j<n && grid[i][j]==1){
+                        q.push({i,j});
+                        grid[i][j]=2;
+                        fresh--;
+                    }
+                }
+            }
+        }
+        return ans;
+
+    }
+    int orangesRotting(vector<vector<int>>& grid) {
         queue<pair<int,int>> q;
         int fresh=0;
-
-        function<void()>bfs=[&](){
-            
-            int dx[4] = {1, -1, 0, 0};
-            int dy[4] = {0, 0, 1, -1};
-            while(q.size()>0 && fresh >0){
-                int size=q.size();
-                ans++;
-                while(size>0){
-                    size--;
-                    int x=q.front().first;
-                    int y=q.front().second;
-                    q.pop();
-                    for(int k=0;k<4;k++){
-                        int nx=x+dx[k];
-                        int ny=y+dy[k];
-                        if(nx>=0 && nx<n && ny>=0 && ny<m && grid[nx][ny]==1){               
-                            q.push({nx,ny});
-                            grid[nx][ny]=2;
-                            fresh--;
-                            
-                        }
-                    }
-
-                }
-                
-            }
-        };
-            for(int i=0;i<n;i++){
-                for(int j=0;j<m;j++){
-                    if(grid[i][j]==2){
-                        q.push({i,j});
-                    }else if(grid[i][j]==1){
-                        fresh++;
-                    }
+        int m=grid.size(),n=grid[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.push({i,j});
+                }else if(grid[i][j]==1){
+                    fresh++;
                 }
             }
-            bfs();
-            return fresh==0?ans:-1;
+        }
+        if(fresh==0) return 0;
+        int ans=bfs(grid,q,fresh);
+        return fresh==0?ans:-1;
     }
 };
