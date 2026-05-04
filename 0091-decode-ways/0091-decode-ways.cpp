4723a687d1bc36946b7876fc0d441c32adbfee5d
thakurpuru <purushottamthakur78@gmail.com>
2026-05-04 21:19:05 +0530

    0091-DECODE-WAYS Stats: Time: 7 ms (5.15%), Space: 11.6 MB (5.16%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     int count;
-    unordered_map<string,char>mp;
-    vector<int>dp;
-    int dfs(int i,string s){
-        if(i==s.size()){
+    unordered_map<string,char> mp;
+    vector<int> dp;
+    int solve(string& s,int i){
+        if(i>=s.size()){
             return 1;
         }
         if(dp[i]!=-1) return dp[i];
         int ways=0;
-        if(i+1<=s.size() && mp.count(s.substr(i,1))){
-            ways+=dfs(i+1,s);
+        if(i+1<s.size() && mp.count(s.substr(i,2))){
+            ways+=solve(s,i+2);
         }
-        if(i+2<=s.size() && mp.count(s.substr(i,2))){
-            ways+=dfs(i+2,s);
-        }
-
-       return dp[i]=ways;
-
-    }
+        if(mp.count(s.substr(i,1)))
+        ways+=solve(s,i+1);
+        return dp[i]=ways;
+    } 
     int numDecodings(string s) {
-        for (int i = 1; i <= 26; i++) {
-            mp[to_string(i)] = 'A' + (i - 1);
+        for(int i=1;i<=26;i++){
+            mp[to_string(i)]='A'+i;
         }
         dp.assign(s.size(),-1);
-        
-        return dfs(0,s);
-
+        return solve(s,0);
     }
 };
