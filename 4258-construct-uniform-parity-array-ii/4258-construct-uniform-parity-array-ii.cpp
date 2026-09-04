f210a2a6e803b4906edfe3121689022a97bfddd2
thakurpuru <purushottamthakur78@gmail.com>
2026-09-04 15:02:34 +0530

    4258-CONSTRUCT-UNIFORM-PARITY-ARRAY-II Stats: Time: 0 ms (100%), Space: 166 MB (25.16%) - LeetHub

--- 4258-construct-uniform-parity-array-ii/4258-construct-uniform-parity-array-ii.cpp
+++ 4258-construct-uniform-parity-array-ii/4258-construct-uniform-parity-array-ii.cpp
@@ -2,21 +2,19 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         int j=0,n=nums1.size();
-        int cnt=0;
+        int cnt=0,omin=INT_MAX,emin=INT_MAX;
         for(int k=0;k<n;k++){
             if(nums1[k]%2!=0){
                 cnt++;
-                if(nums1[k]<nums1[j]) j=k;
+                omin=min(omin,nums1[k]);
+            }else{
+                emin=min(emin,nums1[k]);
             }
             
         }
         if(cnt==0 || cnt==n) return true;
-        int l=0;
-        while(l<n){
-            if(nums1[l]%2==0 && nums1[l]-nums1[j]<1) return false;
-            l++;
-        }
-        return true;
+        if(emin-omin>=1) return true;
+        return false;
 
     }
 };
