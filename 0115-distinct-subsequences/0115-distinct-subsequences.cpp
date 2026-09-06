e8683e085ec0cbba665d10767e3f2efc0fdadeab
thakurpuru <purushottamthakur78@gmail.com>
2026-09-06 13:20:52 +0530

    0115-DISTINCT-SUBSEQUENCES Stats: Time: 36 ms (26.62%), Space: 27.1 MB (45.45%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
+    int solve(int i,int j,string& s,string& t,vector<vector<int>>& dp){
+        if(j==t.size()) return 1;
+        if(i==s.size()) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int ans=0;
+        if(s[i]==t[j]){
+            ans+=solve(i+1,j+1,s,t,dp);
+        }
+        ans+=solve(i+1,j,s,t,dp);
+        return dp[i][j]=ans;
+    }
     int numDistinct(string s, string t) {
-        int n=s.size(),m=t.size();
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-        function<int(int,int)> subsequence=[&](int i,int j){
-            if(j>=m) return 1;
-            if(i>=n) return 0;
-            if(dp[i][j]!=-1) return dp[i][j];
-            int ans=0;
-            
-            if(s[i]==t[j]){
-                
-                ans+=subsequence(i+1,j+1);
-                
-            }
-            ans+=subsequence(i+1,j);
-            return dp[i][j]=ans;
-
-        };
-        
-        return subsequence(0,0);
-        
+        vector<vector<int>> dp(s.size(),vector<int>(t.size(),-1));
+        return solve(0,0,s,t,dp);
     }
 };
